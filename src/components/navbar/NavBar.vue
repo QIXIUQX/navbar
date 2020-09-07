05bd4388cb4664cf8208843c3bd33ae81dbc2614
QIXIUQX <70133122+QIXIUQX@users.noreply.github.com>
2020-09-07 11:53:33 +0800

    '移动端顶部导航'

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,5 +1,9 @@
 <template>
-    $END$
+    <div class="nav-bar">
+        <div class="left"><slot name="left"></slot></div>
+        <div class="center"><slot name="center"></slot></div>
+        <div class="right"><slot name="right"></slot></div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +13,18 @@
 </script>
 
 <style scoped>
+    .nav-bar {
+        display: flex;
+        height: 44px;
+        width: 100%;
+        line-height: 44px;
+        text-align: center;
 
+    }
+    .left,.right{
+        width: 60px;
+    }
+    .center{
+        flex: 1;
+    }
 </style>
